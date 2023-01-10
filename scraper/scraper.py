8721cf70d1c0d783132654e2f2bf12f364430974
Erik M. Dodd <emdodd27@gmail.com>
2023-01-10 15:19:10 -0800

    get_citation_needed_count() function is now working

---
+++ scraper/scraper.py
@@ -0,0 +1,31 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
+# Create function named get_citations_needed_count
+# takes in a url string and returns an integer.
+def get_citations_needed_count(url):
+    soup = BeautifulSoup(response.text, "html.parser")
+    get_citation = soup.findAll('p')
+    citation_count = 0
+    for citation in get_citation:
+        if "citation needed" in citation.getText():
+            citation_count += 1
+    return citation_count
+
+
+#Create function named get_citations_needed_report
+#takes in a url string and returns a report string
+#the string should be formatted with each citation listed in the order found.
+def get_citations_needed_report():
+    pass
+
+
+if __name__ =='__main__':
+    url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+    response = requests.get(url)
+    #results = parse(response.text)
+    #print(results)
+    print(get_citations_needed_count(url))
